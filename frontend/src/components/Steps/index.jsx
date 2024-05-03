9cfe581bbf1f8829c4c8840539bdec69c1ce0e41
RedHeads_Enjoyer <dsc.cxc@mail.ru>
2024-05-03 13:11:06 +0300

    making create recipe page

--- frontend/src/components/Steps/index.jsx
+++ frontend/src/components/Steps/index.jsx
@@ -1,14 +1,14 @@
 import classes from "./styles.module.css";
 import {useEffect, useState} from "react";
-import axios from "axios";
-import {dbUrl} from "../../config";
-import {getJwtAuthHeader} from "../../functions";
 import InputTextArea from "../InputTextArea";
 
 const Steps = ({steps, onChange, name}) => {
+    const [durations, setDurations] = useState([])
 
     const handleAddStepButton = (e) => {
         e.preventDefault()
+        setDurations([...durations, {"h": 0, "m": 0, "s": 0}])
+        console.log(durations)
         onChange({ currentTarget: {name, value: [...steps, {duration: 0, description: ""}]} })
     }
 
@@ -18,22 +18,42 @@ const Steps = ({steps, onChange, name}) => {
     }
 
     const handleDurationHoursChange = (e, index) => {
-        steps[index].duration = parseInt(e.target.value) * 60 * 60
-        onChange({ currentTarget: {name, value: steps}})
+        const newDurations = []
+        for (let i = 0; i < durations.length; i++) {
+            if (i !== index) newDurations.push(durations[i])
+            else newDurations.push({...durations[i], h: parseInt(e.target.value)})
+        }
+        setDurations(newDurations)
     }
 
     const handleDurationMinutesChange = (e, index) => {
-        steps[index].duration = parseInt(e.target.value) * 60
-        onChange({ currentTarget: {name, value: steps}})
+        const newDurations = []
+        for (let i = 0; i < durations.length; i++) {
+            if (i !== index) newDurations.push(durations[i])
+            else newDurations.push({...durations[i], m: parseInt(e.target.value)})
+        }
+        setDurations(newDurations)
     }
 
     const handleDurationSecondsChange = (e, index) => {
-        steps[index].duration = parseInt(e.target.value)
-        onChange({ currentTarget: {name, value: steps}})
+        const newDurations = []
+        for (let i = 0; i < durations.length; i++) {
+            if (i !== index) newDurations.push(durations[i])
+            else newDurations.push({...durations[i], s: parseInt(e.target.value)})
+        }
+        setDurations(newDurations)
     }
 
-
-
+    useEffect(() => {
+        if (durations.length !== 0) {
+            const newSteps = []
+            for (let i = 0; i < steps.length; i++) {
+                newSteps.push({...steps[i], duration: durations[i].h * 3600 + durations[i].m * 60 + durations[i].s})
+            }
+            console.log(newSteps)
+            onChange({ currentTarget: {name, value: newSteps} })
+        }
+    }, [durations])
 
     return (
         <div className={classes.steps__wrapper}>
@@ -45,16 +65,19 @@ const Steps = ({steps, onChange, name}) => {
                         </div>
                         <div className={classes.duration__wrapper}>
                             <input
+                                value={durations[index].h}
                                 className={classes.number__input}
                                 type={"number"}
                                 onChange={(e) => handleDurationHoursChange(e, index)}
                             />
                             <input
+                                value={durations[index].m}
                                 className={classes.number__input}
                                 type={"number"}
                                 onChange={(e) => handleDurationMinutesChange(e, index)}
                             />
                             <input
+                                value={durations[index].s}
                                 className={classes.number__input}
                                 type={"number"}
                                 onChange={(e) => handleDurationSecondsChange(e, index)}
