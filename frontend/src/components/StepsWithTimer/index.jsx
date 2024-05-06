806dfd75fcd562a707afe7f510502bd8360cdb45
RedHeads_Enjoyer <dsc.cxc@mail.ru>
2024-05-06 23:04:21 +0300

    Добавлены этапы при открытии рецепта

--- frontend/src/components/StepsWithTimer/index.jsx
+++ frontend/src/components/StepsWithTimer/index.jsx
@@ -1,22 +1,50 @@
 import classes from "./styles.module.css";
 import InputTextArea from "../InputTextArea";
 import Timer from "../Timer";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
-const StepsWithTimer = ({steps}) => {
-    const handleStepPlayButton = (e, index) => {
-
-    }
+const PlayTimer = ({duration}) => {
+    const [time, setTime] = useState(duration)
 
     return (
+        <div className={classes.duration__wrapper}>
+            <div className={classes.number__wrapper}>
+                <p>{Math.floor(time / 3600)}</p>
+                <label>час</label>
+            </div>
+            <div className={classes.number__wrapper}>
+                <p>{Math.floor(time % 3600 / 60)}</p>
+                <label>мин</label>
+            </div>
+            <div className={classes.number__wrapper}>
+                <p>{time % 60}</p>
+                <label>сек</label>
+            </div>
+        </div>
+    )
+}
+
+
+const StepsWithTimer = ({steps}) => {
+    return (
         <div className={classes.steps__wrapper}>
-            asd
             {steps !== undefined ? steps.map((step, index) => (
                 <div key={`step${index}`}>
-                    <p className={classes.step__description}>{step.description}</p>
+                    <div className={classes.step__wrapper}>
+                        <div className={classes.header__wrapper}>
+                            <div className={classes.index__wrapper}>
+                                <p>{index}</p>
+                            </div>
+                            <PlayTimer duration={step.duration}/>
+                        </div>
+                        <div className={classes.description__wrapper}>
+                            <p className={classes.description}>{step.description}</p>
+                        </div>
+                    </div>
+
                 </div>
 
-            )) : "net"}
+            )) : ""}
         </div>
     );
 };
