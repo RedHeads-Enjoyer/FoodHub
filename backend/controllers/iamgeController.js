4ef062df7fa9d6a7172617e7fb6e01577daeae9d
RedHeads_Enjoyer <dsc.cxc@mail.ru>
2024-05-09 23:35:31 +0300

    Изменена ссылка на сервер в fromntend

--- backend/controllers/iamgeController.js
+++ backend/controllers/iamgeController.js
@@ -1,11 +1,17 @@
 const path = require('path')
+const fs = require('fs');
 
 class imageController {
     async getOneByName(req, res) {
         try {
             const { name } = req.params;
             const filePath = path.join(__dirname, '../uploads', name);
-            res.sendFile(filePath);
+            fs.access(filePath, fs.constants.F_OK, (err) => {
+                if (err) res.sendFile(filePath);
+                const filePath = path.join(__dirname, '../uploads', "image_placeholder.svg");
+                res.sendFile(filePath);
+            });
+
         } catch (e) {
             res.status(500).json({ message: "Ошибка при получении файла" });
         }
